10d0b0595a58bc0d1cd4a1657b98c2901c647581
trnKhanh <tnk29082003@gmail.com>
2023-03-31 13:27:16 +0700

    add crawl watch items to crawl database

--- src/crawler/spiders/tgdd/watch_spider.py
+++ src/crawler/spiders/tgdd/watch_spider.py
@@ -1,6 +1,6 @@
 from . import tgdd_spider
 import re
-from . import tgdd_utils
+from .tgdd_utils import *
 
 class WatchSpider(tgdd_spider.TgddSpider):
     name = "tgdd_watch"
@@ -17,19 +17,25 @@ class WatchSpider(tgdd_spider.TgddSpider):
         if price: 
             price = re.sub(r"\D", "", price)
         # parse product parameter
-        screen = ', '.join(response.xpath(tgdd_utils.parameter_xpath("Màn hình")).getall())
-        brand = ', '.join(response.xpath(tgdd_utils.parameter_xpath("Hãng")).getall())
+        screen = ', '.join(response.xpath(parameter_xpath("Màn hình")).getall())
+        brand = ', '.join(response.xpath(parameter_xpath("Hãng")).getall())
         url = response.request.url
 
         # yield result of the current product
-        yield {
-            "name": name,
-            "price": price,
-            "screen": screen,
-            "brand": brand,
-            "url": url
-        }
-
+        # yield {
+        #     "name": name,
+        #     "price": price,
+        #     "screen": screen,
+        #     "brand": brand,
+        #     "url": url
+        # }
+        sql = """
+            INSERT INTO watch (name, price, screen, brand, url) 
+            VALUES (%s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE id=id
+        """
+        crawl_cursor.execute(sql, (name, price, screen, brand, url))
+        crawl_db.commit()
         # follow the link to other products
         yield from self.product_follow(response)
         
