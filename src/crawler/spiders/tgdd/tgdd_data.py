8de6437e60a9acf155991f02188b2609a12e231d
trnKhanh <tnk29082003@gmail.com>
2023-04-01 22:04:20 +0700

    redesign tgdd_spider

---
+++ src/crawler/spiders/tgdd/tgdd_data.py
@@ -0,0 +1,78 @@
+category_table = {
+    44: "laptop",
+    5698: "PC",
+    4547: "keyboard",
+    86: "mouse",
+    5693: "printer",
+    5697: "screen",
+    522: "tablet",
+    42: "phone",
+    7077: "smart_watch"
+}
+def get_category_table(id):
+    if id in category_table:
+        return category_table[id]
+    else:
+        return None
+    
+category_id = {}
+for id in category_table:
+    category_id[category_table[id]] = id
+
+category_parameter = {
+    category_id["laptop"]: {
+        "cpu": "CPU",
+        "ram": "RAM",
+        "disk": "Ổ cứng",
+        "screen": "Màn hình",
+        "OS": "Hệ điều hành",
+    },
+    category_id["PC"]: {
+        "cpu": "CPU",
+        "ram": "RAM",
+        "disk": "Ổ cứng",
+        "screen": "Màn hình",
+        "OS": "Hệ điều hành",
+    },
+    category_id["keyboard"]: {
+        "compatible": "Tương thích",
+        "connect_type": "Cách kết nối",
+        "size": "Số phím",
+        "brand": "Hãng",
+    },
+    category_id["mouse"]: {
+        "dpi": "Độ phân giải",
+        "connect_type": "Cách kết nối",
+        "brand": "Hãng",
+    },
+    category_id["printer"]: {
+        "printer_type": 'Loại máy',
+        "print_speed": 'Tốc độ in',
+        "ink_type": 'Loại mực in',
+        "print_quality": 'Chất lượng in',
+        "paper_type": 'Giấy in',
+        "brand": "Hãng",
+    },
+    category_id["screen"]: {
+        "screen_type": 'Loại màn hình',
+        "screen_size": 'Màn hình',
+    },
+    category_id["tablet"]: {
+        "chip": "Chip",
+        "ram": "RAM",
+        "disk": "Dung lượng lưu trữ",
+        "screen": "Màn hình",
+        "OS": "Hệ điều hành",
+    },
+    category_id["phone"]: {
+        "chip": "Chip",
+        "ram": "RAM",
+        "disk": "Dung lượng lưu trữ",
+        "screen": "Màn hình",
+        "OS": "Hệ điều hành",
+    },
+    category_id["smart_watch"]: {
+        "screen": "Màn hình",
+        "brand": "Hãng",
+    }
+}
--- src/crawler/spiders/tgdd/tgdd_spider.py
+++ src/crawler/spiders/tgdd/tgdd_spider.py
@@ -3,10 +3,24 @@ import re
 from scrapy.http import HtmlResponse
 import json
 from urllib.parse import urlencode
+from .tgdd_utils import *
+from .tgdd_data import *
+import regex as re
 
 class TgddSpider(scrapy.Spider):
     name = 'tgdd'
-    urls = []
+    urls = [
+        'https://www.thegioididong.com/Category/FilterProductBox?c=44&o=17&priceminmax=0-1000000&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=5698&o=7&priceminmax=0-1000000&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=4547&o=14&priceminmax=0-1000000&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=86&o=14&priceminmax=0-1000000&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=5693&priceminmax=0-1000000&o=13&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=5697&priceminmax=0-1000000&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=522&o=17&priceminmax=0-1000000&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=42&o=17&priceminmax=0-1000000&pi=0',
+        # 'https://www.thegioididong.com/Category/FilterProductBox?c=7077&o=17&priceminmax=0-1000000&pi=0',
+
+    ]
     post_data = urlencode({"IsParentCate": False, "IsShowCompare": True, "prevent": True})
 
     def start_requests(self):
@@ -20,15 +34,22 @@ class TgddSpider(scrapy.Spider):
                 body=self.post_data, 
                 callback=self.parse,
             )
+        
 
     def parse(self, response):
         data = json.loads(response.text)
         products = HtmlResponse(url="https://www.thegioididong.com", body=data["listproducts"], encoding="utf-8")
         
+        id = re.search(r'(?<=(c=))\d*', response.request.url)
+        if id:
+            id = int(id.group())
+        else: 
+            id = -1
+
         # find and follow to product pages
         for product_link in products.xpath("//li/a[1]/@href").getall():
             url = response.urljoin(product_link)
-            yield scrapy.Request(url=url, callback=self.product_parse)
+            yield scrapy.Request(url=url, callback=self.product_parse, meta={"id": id})
 
         # request more data from tgdd's database
         for pi in range(20, data["total"], 20):
@@ -41,11 +62,54 @@ class TgddSpider(scrapy.Spider):
                 body=response.request.body, 
                 callback=self.parse,
             )
+
     def product_parse(self, response):
-        pass
+        product_box = response.xpath("//section[contains(@class, 'detail')]")
+        if not product_box:
+            return
+        # id = int(product_box.xpath("self::*/@data-cate-id").get())
+        id = response.meta.get("id")
+        parameter_list = [] # store name of parameter
+        parameter_data = [] # store the data about corresponding parameter
+
+        # product name
+        name = response.xpath("//h1/text()").get()
+        parameter_list.append("name")
+        parameter_data.append(f'"{name}"')
+
+        # product price
+        price = response.xpath("//*[contains(@class, 'box-price-present')]/text()").get()
+        # normalize product price to integer
+        if price: 
+            price = re.sub(r"\D", "", price)
+        parameter_list.append("price")
+        parameter_data.append(price)
+
+        # parse product parameter
+        for parameter_name in category_parameter[id]:
+            data = ', '.join(product_box.xpath(parameter_xpath(category_parameter[id][parameter_name])).getall())
+            parameter_list.append(parameter_name)
+            parameter_data.append(f"'{data}'")
+        # parse product url
+        url = response.request.url
+        parameter_list.append("url")
+        parameter_data.append(f"'{url}'")
+        yield {
+            "name": name
+        }
+        # add data to database
+        sql = f"""
+            INSERT INTO {get_category_table(id)} ({', '.join(parameter_list)}) 
+            VALUES ({("%s," * len(parameter_list)).strip(',')})
+            ON DUPLICATE KEY UPDATE id=id
+        """
+        crawl_cursor.execute(sql, parameter_data)
+        crawl_db.commit()
+        # follow the link to other products
+        yield from self.product_follow(response)
     
     def product_follow(self, response):
         for next_page in response.xpath("(//*[@class='box03 group desk'])[1]/a/@href").getall():
             url = response.urljoin(next_page)
-            yield scrapy.Request(url=url, callback=self.product_parse)
+            yield scrapy.Request(url=url, callback=self.product_parse, meta=response.meta)
         
