2a953b96e850a4b7a4c5d695679944dc56191553
trnKhanh <tnk29082003@gmail.com>
2023-04-01 22:05:05 +0700

    change main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,4 @@
-from crawler.spiders.tgdd.computer_spider import ComputerSpider as TGDDComputerSpider
-from crawler.spiders.tgdd.tablet_spider import TabletSpider as TGDDTabletSpider
-from crawler.spiders.tgdd.printer_spider import PrinterSpider as TGDDPrinterSpider
-from crawler.spiders.tgdd.watch_spider import WatchSpider as TGDDWatchSpider
-from crawler.spiders.tgdd.screen_spider import ScreenSpider as TGDDScreenSpider
-from crawler.spiders.tgdd.mouse_spider import MouseSpider as TGDDMouseSpider
-from crawler.spiders.tgdd.keyboard_spider import KeyboardSpider as TGDDKeyboardSpider
+from crawler.spiders.tgdd.tgdd_spider import TgddSpider
 from crawler.spiders.tgdd.all_spider import AllSpider as TGDDAllSpider
 
 from scrapy.crawler import CrawlerProcess
@@ -21,14 +15,8 @@ if __name__ == '__main__':
     if os.path.isfile('tgdd.log'):
         os.remove('tgdd.log')
     process = CrawlerProcess(get_project_settings()) 
-    process.crawl(TGDDComputerSpider)
-    process.crawl(TGDDTabletSpider)
-    process.crawl(TGDDPrinterSpider)
-    process.crawl(TGDDScreenSpider)
-    process.crawl(TGDDWatchSpider)
-    process.crawl(TGDDMouseSpider)
-    process.crawl(TGDDKeyboardSpider)
-    d = process.join()
-    d.addCallback(lambda _: foo(process))
+    process.crawl(TgddSpider)
+    # d = process.join()
+    # d.addCallback(lambda _: foo(process))
 
     process.start()
